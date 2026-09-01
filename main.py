ad21ea6c48675138a61fffdf8228c81924f0562b
P4l1ndr0me <joshuayjm@gmail.com>
2026-08-31 21:32:14 -0400

    Add some random commands

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import logging
 from dotenv import load_dotenv
 import os
 
+import random
+import asyncio
+
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 
@@ -13,20 +16,53 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+channel_id = os.getenv("CHANNEL_ID") # josh's channel for testing
+def in_channel(ctx):
+    return ctx.channel.id == channel_id
+
 @bot.event
 async def on_ready():
     print("running")
 
 @bot.command()
+@commands.check(in_channel)
 async def hi(ctx):
     await ctx.send(f"Hey {ctx.author.mention}! Welcome to MCC!")
 
 @bot.command()
+@commands.check(in_channel)
 async def repeat(ctx, *, arg):
     await ctx.send(arg)
 
 @bot.command()
+@commands.check(in_channel)
 async def problem(ctx):
-    await ctx.send("What is 1+1?")
+    operations = ("+", "-", "*")
+    num1 = random.randint(1, 10)
+    num2 = random.randint(1, 10)
+    operation = random.choice(operations)
+
+    if operation == "+":
+        answer = num1 + num2
+    elif operation == "-":
+        answer = num1 - num2
+    else:
+        answer = num1 * num2
+
+    await ctx.send(f"What is {num1} {operation} {num2}?")
+
+    def check(message):
+        return message.author == ctx.author and message.channel == ctx.channel
+
+    try:
+        response = await bot.wait_for("message", timeout=30.0, check=check)
+    except asyncio.TimeoutError:
+        await ctx.send("Ran out of time!")
+    else:
+        if response.content == str(answer):
+            await ctx.send("Correct!")
+        else:
+            await ctx.send(f"Incorrect! The answer was {answer}")
+
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
