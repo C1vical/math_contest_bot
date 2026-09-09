a7b4288b9550526c53016d73d77817147d7fd69e
Jonathan <jonathanyjh@gmail.com>
2026-09-08 21:14:52 -0400

    Edited load_contest function to adjust for previous changes

--- scraper.py
+++ scraper.py
@@ -1,38 +1,43 @@
 import asyncio
 import random
-from parsing.aops_parser import fetch_aops_problem_set
+from parsing.aops_parser import fetch_problem_statement
 from rendering.aops_renderer import render_all_problems
-from database import create_database, add_problem, get_loaded_contests
+from database import create_database, add_problem, get_loaded_problems
 from constants import CONTEST_REGISTRY, get_contest_info
 from logger_config import get_file_logger
 
 logger = get_file_logger("parsing", "logs/parsing.log")
 
-async def load_contest(year: int, contest: str):
+async def load_contest(year: int, contest: str, loaded_problems: set):
     """Fetch a full problem set for a contest year from AoPS and store in SQLite."""
     info = get_contest_info(year, contest)
     wiki_title, _, _, max_probs = info
-    problems = await fetch_aops_problem_set(year, wiki_title)
 
-    for q_num, statement in enumerate(problems[:max_probs], start=1):
-        add_problem(year, contest, q_num, statement)
+    for problem_num in range(1, max_probs + 1):
+        if (year, contest, problem_num) in loaded_problems:
+            continue
+        question_statement = await fetch_problem_statement(year, wiki_title, problem_num)
 
-    logger.info(f"Successfully loaded {year} {contest} ({len(problems[:max_probs])} problems)")
+        if not question_statement:
+            logger.warning(f"Failed to load {year} {contest} Problem {problem_num}")
+            continue
+
+        add_problem(year, contest, problem_num, question_statement)
+        logger.info(f"Successfully loaded {year} {contest} Problem {problem_num}")
+        await asyncio.sleep(random.uniform(2.5, 4.0))
+
+    logger.info(f"Successfully loaded {year} {contest} ({max_probs} problems)")
 
 async def load_all_contests():
     """Iterate through all registered contests and fetch missing problem sets into SQLite."""
-    loaded_set = get_loaded_contests()
+    loaded_problems = get_loaded_problems()
 
     for key, info in CONTEST_REGISTRY.items():
         contest = key.split("_")[0]
         _, min_year, max_year, _ = info
 
         for year in range(min_year, max_year + 1):
-            if (year, contest) in loaded_set:
-                continue
-
-            await load_contest(year, contest)
-            await asyncio.sleep(random.uniform(4.0, 7.0))
+            await load_contest(year, contest, loaded_problems)
 
 async def prepare_database_and_renders():
     """Creates database and runs scraper and renderer concurrently."""
