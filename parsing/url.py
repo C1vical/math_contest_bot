27ade47adbafc631bcb4f20beaa65e58702cead0
P4l1ndr0me <joshuayjm@gmail.com>
2026-09-10 21:09:57 -0400

    Update url.py to add other Waterloo contests (Euclid, FGH, etc.)

--- parsing/url.py
+++ parsing/url.py
@@ -5,7 +5,8 @@ from bs4 import BeautifulSoup
 
 def extract_urls():
     base_url = "https://cemc.uwaterloo.ca/resources/past-contests"
-    contests = ["Pascal", "Cayley", "Fermat"]
+    view_query = "block_config_key%3Dpast_contest%3A1srMgUG8ZWnN5_Mx6CmP5HeP-aleHwL0jgxyVUuVYE4"
+    contests = ["Pascal", "Cayley", "Fermat", "Euclid", "Hypatia", "Galois", "Fryer", "Gauss7", "Gauss8"]
     academic_years = {
         "2026": "86",
         "2025": "83",
@@ -19,31 +20,40 @@ def extract_urls():
         "2017": "37",
         "2016": "38",
     }
+
+    contest_categories = {
+        "PCF": "14",
+        "Euclid": "24",
+        "FGH": "25",
+        "Gauss": "13"
+    }
+
     urls = {}
 
     for academic_year in academic_years.values():
-        query_params = {
-            "view_query": "block_config_key%3Dpast_contest%3A1srMgUG8ZWnN5_Mx6CmP5HeP-aleHwL0jgxyVUuVYE4",
-            "grade": "All",
-            "academic_year": academic_year,
-            "contest_category": 14,
-        }
+        for contest_category in contest_categories.values():
+            query_params = {
+                "view_query": view_query,
+                "grade": "All",
+                "academic_year": academic_year,
+                "contest_category": contest_category,
+            }
 
-        response = requests.get(base_url, params=query_params)
-        soup = BeautifulSoup(response.text, "html.parser")
+            response = requests.get(base_url, params=query_params)
+            soup = BeautifulSoup(response.text, "html.parser")
 
-        for tag in soup.find_all("a", href=re.compile("Contest.html")):
-            link = "https://cemc.uwaterloo.ca" + tag["href"]
-            id = tag["href"].split("/")[-1]
+            for tag in soup.find_all("a", href=re.compile("Contest.html")):
+                link = "https://cemc.uwaterloo.ca" + tag["href"]
+                id = tag["href"].split("/")[-1]
 
-            for contest in contests:
-                if contest in id:
-                    c = contest
-            for year in academic_years.keys():
-                if year in id:
-                    y = year
+                for contest in contests:
+                    if contest in id:
+                        c = contest
+                for year in academic_years.keys():
+                    if year in id:
+                        y = year
 
-            urls[(c,y)] = link
+                urls[(c,y)] = link
     return urls
 if __name__ == "__main__":
     dict = extract_urls()
